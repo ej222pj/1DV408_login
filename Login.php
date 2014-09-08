e36a5bf11f520d09bafd1918c646615e281c7843
ej222pj <ej222pj@student.lnu.se>
2014-09-08 20:01:00 +0200

    Påbörjat login klassen

--- Login.php
+++ Login.php
@@ -9,8 +9,21 @@ class Login{
 	
 	public function inputHtml(){
 		
-		echo "Hej från InputHtml!";
-		
+		?>
+			<!DOCTYPE HTML>
+			<html>
+				<head>
+					<meta charset="UTF-8">
+					<meta name="viewport" content="width=device-width, initial-scale=1">
+					<title>Lab2 1DV408_Login EJ222PJ</title>
+				</head>
+				<body>
+					<h1>Lab2 1DV408_Login EJ222PJ</h1>
+					<h2>Ej Inloggad</h2>
+					
+				</body>
+			</html>	
+		<?php	
 	}
 	
 	
