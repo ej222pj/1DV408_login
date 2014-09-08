dfcdf1a633717e9fe09a78db8dda4faa937efb68
ej222pj <ej222pj@student.lnu.se>
2014-09-08 19:26:45 +0200

    Skapat index.php

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-Nu funkar github och de läggs upp på http://127.0.0.1/1DV408_login/
-    </body>
-</html>
+<?php
+require_once('login.php');
+$Login = new Login();
+$Login->inputHtml();
