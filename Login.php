9fe34c79508bc9622d457048c9c783a80764c09a
ej222pj <ej222pj@student.lnu.se>
2014-09-08 20:16:43 +0200

    Gjort ett skelett för inloggning

--- Login.php
+++ Login.php
@@ -20,7 +20,18 @@ class Login{
 				<body>
 					<h1>Lab2 1DV408_Login EJ222PJ</h1>
 					<h2>Ej Inloggad</h2>
-					
+					<form action="?login" method="post" enctype="multipart/form-data">
+						<fieldset>
+							<legend>Login - Skriv in användarnamn och lösenord</legend>
+							<lable for="username">Användarnamn:</lable>
+							<input type="text" name="username" value=""/>
+							<lable for="password">Lösenord:</lable>
+							<input type="password" name="password" id="password"/>
+							<lable for="cookie">Håll mig inloggad:</lable>
+							<input type="checkbox" id="cookie" name="cookie" value="yes"/>
+							<input type="submit" value="Logga in"/>
+						</fieldset>
+					</form>
 				</body>
 			</html>	
 		<?php	
